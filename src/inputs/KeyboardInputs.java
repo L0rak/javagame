f05684c833839895404ed8009e1e0313019347fe
L0rak <60035309+L0rak@users.noreply.github.com>
2023-04-15 21:38:02 +0200

    Finished the gravity engine

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -34,6 +34,9 @@ public class KeyboardInputs implements KeyListener {
             case KeyEvent.VK_D:
             gamePanel.getGame().getPlayer().setRight(true);
                 break;
+            case KeyEvent.VK_SPACE:
+                gamePanel.getGame().getPlayer().setJump(true);
+                break;
             case KeyEvent.VK_F1:
                 gamePanel.getGame().getPlayer().setPlayerClass(0);
                 break;
@@ -57,6 +60,9 @@ public class KeyboardInputs implements KeyListener {
                 break;
             case KeyEvent.VK_D:
                 gamePanel.getGame().getPlayer().setRight(false);
+                break;
+            case KeyEvent.VK_SPACE:
+                gamePanel.getGame().getPlayer().setJump(false);
                 break;
         }
     }
--- src/main/Game.java
+++ src/main/Game.java
@@ -17,7 +17,7 @@ public class Game implements Runnable{
     private LevelManager levelManager;
 
     public final static int TILES_DEFAULT_SIZE = 32;
-    public final static float SCALE = 2.5f;
+    public final static float SCALE = 2f;
     public final static int TILES_IN_WIDTH = 26;
     public final static int TILES_IN_HEIGHT = 14;
     public final static int TILES_SIZE = (int) (TILES_DEFAULT_SIZE * SCALE);
@@ -36,7 +36,7 @@ public class Game implements Runnable{
 
     private void initClasses() {
         levelManager = new LevelManager(this);
-        player = new Player(200,300, (int)(64 * SCALE), (int) (40 * SCALE));
+        player = new Player(200,200, (int)(64 * SCALE), (int) (40 * SCALE));
         player.loadLvlData(levelManager.getCurrentLevel().getLevelData());
     }
 
